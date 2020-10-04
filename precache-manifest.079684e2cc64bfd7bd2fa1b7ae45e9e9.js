37b790b6c643f5920b2fcd131da0632f4e5dcdfb
sidoosh <dhanwani.nainanew@gmail.com>
2020-10-05 03:48:37 +0530

    Updates

---
+++ precache-manifest.079684e2cc64bfd7bd2fa1b7ae45e9e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf22422baf762b1a14a8ef113339f34",
+    "url": "/birthday-cal/index.html"
+  },
+  {
+    "revision": "7b359aad7cce9799ffcd",
+    "url": "/birthday-cal/static/css/main.8c700954.chunk.css"
+  },
+  {
+    "revision": "1e5c5455dbef21a41d48",
+    "url": "/birthday-cal/static/js/2.64c0921b.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/birthday-cal/static/js/2.64c0921b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b359aad7cce9799ffcd",
+    "url": "/birthday-cal/static/js/main.2ff1f4bc.chunk.js"
+  },
+  {
+    "revision": "0d8a84409bda3b644afb",
+    "url": "/birthday-cal/static/js/runtime-main.b6a7f4cd.js"
+  },
+  {
+    "revision": "cd95aa0aab00896339a5b7deaffa30f3",
+    "url": "/birthday-cal/static/media/loader.cd95aa0a.png"
+  }
+]);
